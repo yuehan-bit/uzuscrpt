46aebbe7a79f599a4089a51722e523fad451fc12
yuehan-bit <johnmarkcalimbo90@gmail.com>
2025-04-20 09:32:58 +0800

    Update uzuscrpt.lua

--- uzuscrpt.lua
+++ uzuscrpt.lua
@@ -362,6 +362,8 @@ misc_folder:AddToggle({text = "Auto Rejoin", state = config.auto_rejoin, callbac
     if v then auto_rejoin() end
 end})
 
+misc_folder:AddLabel("🛡️ Anti Kick: Active")
+
 misc_folder:AddBind({text = "Toggle GUI", key = "LeftControl", callback = function()
     library:Close()
 end})
